a65d205ef5bd5ee53b10bb96ebc7f078e5440d64
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2022-06-11 22:33:28 -0300

    Exibir dados dos sensores ordenado

--- Exemplos/catapintinsat/receptor/receptor.ino
+++ Exemplos/catapintinsat/receptor/receptor.ino
@@ -34,13 +34,11 @@ struct bmp_package{
 typedef struct mq135_package MQ135_package;
 typedef struct accelerometer_package Accelerometer_package;
 typedef struct bmp_package BMP_package;
-
+int next_type = 0;
 
 /* Rádio entre o transmissor e receptor */
 RF24 radio(9,10);  /* Certifique-se que estão conectados nos mesmos pinos */
-const uint64_t pipes[2] = { 
-  0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
-
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 
 void setup(){
   Serial.begin(9600); /* Na velocidade 9600 no Monitor Serial */
@@ -60,49 +58,66 @@ void loop(void){
       listening_accelerometer();
     }else if(len == sizeof(BMP_package)){
       listening_bmp();
-    } 
+    }
   }
-  delay(500);
 }
 
 void listening_accelerometer(){
   Accelerometer_package data;
   radio.read(&data, sizeof(data));
 
-//Envia valores lidos do acelerômetro
-  Serial.println("|  Acelerômetro"); 
-  Serial.print("|  Acel:"); 
-  Serial.print("  X:");Serial.print(data.acelX);
-  Serial.print(" Y:");Serial.print(data.acelY);
-  Serial.print(" Z:");Serial.println(data.acelZ);
-  
-  //Envia valores lidos do giroscópio
-  Serial.print("|  Giro:"); 
-  Serial.print("  X:");Serial.print(data.giroX);
-  Serial.print(" Y:");Serial.print(data.giroY);
-  Serial.print(" Z:");Serial.println(data.giroZ);
-  Serial.println("|"); 
+  if(next_type == 1){
+    Serial.println("|  Acelerômetro"); 
+    Serial.print("|  Acel:"); 
+    Serial.print("  X:");Serial.print(data.acelX);
+    Serial.print(" Y:");Serial.print(data.acelY);
+    Serial.print(" Z:");Serial.println(data.acelZ);
+    
+    Serial.print("|  Giro:"); 
+    Serial.print("  X:");Serial.print(data.giroX);
+    Serial.print(" Y:");Serial.print(data.giroY);
+    Serial.print(" Z:");Serial.println(data.giroZ);
+    Serial.println("|");
+
+    set_step(data.type);
+  }
 }
 
 void  listening_bmp(){
   BMP_package data; 
   radio.read(&data, sizeof(data));
-  Serial.println("|  BMP "); 
-  Serial.print("|  Temperatura: "); Serial.print(data.Temperatura);Serial.println("°C");
-  Serial.print("|  Altitude: "); Serial.print(data.Altitude);Serial.println("m");
-  Serial.print("|  Pressão Atmosférica: "); Serial.print(data.Pressao);Serial.println(" Pa");
-  Serial.println("|"); 
+
+  if(next_type == 2){  
+    Serial.println("|  BMP "); 
+    Serial.print("|  Temperatura: "); Serial.print(data.Temperatura);Serial.println("°C");
+    Serial.print("|  Altitude: "); Serial.print(data.Altitude);Serial.println("m");
+    Serial.print("|  Pressão Atmosférica: "); Serial.print(data.Pressao);Serial.println(" Pa");
+    Serial.println("|");
+
+    set_step(data.type);
+  }
 }
 
 void listening_MQ135(){
   MQ135_package data;
   radio.read(&data, sizeof(data));
 
-  Serial.println("|  MQ135 "); 
-  Serial.print("|  Monóxido de carbono: "); Serial.println(data.CO);
-  Serial.print("|  Dióxido de carbono: "); Serial.println(data.CO2 + 400);
-  Serial.print("|  Álcool: "); Serial.println(data.Alcohol);
-  Serial.print("|  Amónio: "); Serial.println(data.NH4);
-  Serial.print("|  Acetona: "); Serial.println(data.Acetona);
-  Serial.println("|"); 
+  if(next_type == 0){
+    Serial.println("|  MQ135 "); 
+    Serial.print("|  Monóxido de carbono: "); Serial.println(data.CO);
+    Serial.print("|  Dióxido de carbono: "); Serial.println(data.CO2 + 400);
+    Serial.print("|  Álcool: "); Serial.println(data.Alcohol);
+    Serial.print("|  Amónio: "); Serial.println(data.NH4);
+    Serial.print("|  Acetona: "); Serial.println(data.Acetona);
+    Serial.println("|"); 
+    
+    set_step(data.type);
+  }
+}
+
+void set_step(int step){
+  next_type = step + 1;
+  if(next_type > 2){
+    next_type = 0;
+  }
 }
